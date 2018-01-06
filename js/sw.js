ef6bdf85bf9274a5f2745fa5962a24493cf3b3ff
Mohammad RAHMAT <mrahmat_public@hotmail.com>
2018-01-07 02:21:40 +0300

    err handler sw.js

--- js/sw.js
+++ js/sw.js
@@ -30,5 +30,7 @@ self.addEventListener('activate', function(event) {
 self.addEventListener('fetch', function(event) {
 	event.respondWith(caches.match(event.request).then(function(response) {
 		return response || fetch(event.request);
+	}).catch(function(err) {
+		console.log(err);
 	}));
 });

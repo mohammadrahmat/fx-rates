8ac64a4601ee19f987b74e382ae22e280cec63e9
Mohammad RAHMAT <mrahmat_public@hotmail.com>
2018-01-07 01:05:17 +0300

    added missing ) in sw.js

--- js/sw.js
+++ js/sw.js
@@ -39,9 +39,7 @@ self.addEventListener('fetch', function(event) {
 		}
 	}
 
-	event.respondWith(
-		caches.match(event.request).then(function(response) {
-			return response || fetch(event.request);
-		});
-	);
+	event.respondWith(caches.match(event.request).then(function(response) {
+		return response || fetch(event.request);
+	}));
 });

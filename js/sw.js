dec46fc517e819e992c966bf6fd8469250b61939
Mohammad RAHMAT <mrahmat_public@hotmail.com>
2018-01-07 02:35:57 +0300

    sw.js updates

--- js/sw.js
+++ js/sw.js
@@ -3,6 +3,7 @@ var staticCacheName = "fx-rates-v1";
 self.addEventListener('install', function(event) {
 	event.waitUntil(caches.open(staticCacheName).then(function(cache) {
 		return cache.addAll([
+			'./',
 			'js/currency_list.js',
 			'jquery/jquery-3.2.1.min.js',
 			'jquery-migrate-1.4.1.min.js',
@@ -11,30 +12,14 @@ self.addEventListener('install', function(event) {
 			'css/main.css'
 		]);
 	}).catch(function(err) {
-		console.log(`install: 
-						${err}`);
+		console.log(`install: ${err}`);
 	}));
-});
-
-self.addEventListener('activate', function(event) {
-	event.waitUntil(
-		caches.keys().then(function(cacheNames) {
-			return Promise.all(
-				cacheNames.filter(function(cacheName) {
-					return cacheName.startsWith('fx-');
-				}).map(function(cacheName) {
-					return caches.delete(cacheName);
-				})
-			);
-		})
-	);
 });
 
 self.addEventListener('fetch', function(event) {
 	event.respondWith(caches.match(event.request).then(function(response) {
 		return response || fetch(event.request);
 	}).catch(function(err) {
-		console.log(`install: 
-						${err}`);
+		console.log(`fetch: ${err}`);
 	}));
 });

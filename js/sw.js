78f4a20642c29c2c736c77879918ee2a30d4ef0c
Mohammad Rahmat <mrahmat_public@hotmail.com>
2018-01-07 11:37:48 +0300

    Update sw.js

--- js/sw.js
+++ js/sw.js
@@ -1,32 +1,39 @@
-var staticCacheName = "fx-rates-v1";
+const staticCacheName = "fx-rates-v1";
+const expectedCaches = [
+	staticCacheName
+];
+const thingsToCache = [
+	'../index.html',
+	'../css/main.css',
+	'../js/currency_list.js',
+	'../jqm/jquery.mobile-1.5.0-alpha.1.min.js',
+	'../jqm/jquery.mobile-1.5.0-alpha.1.min.css',
+	'../jqm/images/ajax-loader.gif',
+	'../jquery/jquery-3.2.1.min.js',
+	'../jquery/jquery-migrate-1.4.1.min.js',
+	'https://fonts.googleapis.com/css?family=Lobster+Two'
+];
 
-self.addEventListener('install', function(event) {
-	event.waitUntil(caches.open(staticCacheName).then(function(cache) {
-		return cache.addAll([
-			'../index.html',
-			'../css/main.css',
-			'../js/currency_list.js',
-			'../jqm/jquery.mobile-1.5.0-alpha.1.min.js',
-			'../jqm/jquery.mobile-1.5.0-alpha.1.min.css',
-			'../jqm/images/ajax-loader.gif',
-			'../jquery/jquery-3.2.1.min.js',
-			'../jquery/jquery-migrate-1.4.1.min.js',
-			'https://fonts.googleapis.com/css?family=Lobster+Two'
-		]);
-	}).catch(function(err) {
-		console.log(`install: ${err}`);
-	}));
+self.addEventListener('install', event => {
+	self.skipWaiting();
+	event.waitUntil(caches.open(staticCacheName).then(cache => cache.addAll(thingsToCache));
+});
+	
+self.addEventListener('activate', event => {
+	event.waitUntil(
+		caches.keys().then(keys => Promise.all(
+			keys.map(key => {
+				if(!expectedCaches.includes(key)) return caches.delete(key);
+			})
+		))
+	);
 });
 
-self.addEventListener('fetch', function(event) {
-	var requestUrl = new URL(event.request.url);
+self.addEventListener('fetch', event => {
+	const requestUrl = new URL(event.request.url);
 	if (requestUrl.origin === location.origin) {
 		event.respondWith(caches.match('../index.html'));
 		return;
 	}
-	event.respondWith(caches.match(event.request).then(function(response) {
-		return response || fetch(event.request);
-	}).catch(function(err) {
-		console.log(`fetch: ${err}`);
-	}));
+	event.respondWith(caches.match(event.request).then(r => r || fetch(event.request));
 });
